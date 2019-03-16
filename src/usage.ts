d8ad8445f13dd9237fd2b372d5cf60735e5c3768
Benjamin Kotte <bko@bennis-MBP.fritz.box>
2019-03-16 23:37:16 +0100

    remove log

--- src/usage.ts
+++ src/usage.ts
@@ -13,6 +13,5 @@ export function usage(startUsage: NodeJS.CpuUsage): ressources {
   const up = uptime() * 1000;
   const cpuUsedStr = ((system + user) / (up * 10)).toFixed(2);
   const cpuUsed = parseFloat(cpuUsedStr)
-  console.log(rss, heapTotal, heapUsed, cpuUsed);
   return { rss, heapTotal, heapUsed, cpuUsed };
 }
