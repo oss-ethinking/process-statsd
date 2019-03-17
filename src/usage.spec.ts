97a153563c1887cede31eff156cd3ffd7c8bb552
Benjamin Kotte <bko@bennis-MBP.fritz.box>
2019-03-17 20:55:33 +0100

    respect core count

--- src/usage.spec.ts
+++ src/usage.spec.ts
@@ -1,9 +1,10 @@
 import { usage } from './usage';
 import { cpuUsage } from 'process';
+import { cpus } from 'os';
 
-test('usage.cpuUsed between 0 and 100', () => {
+test('usage.cpuUsed between 0 and 100*cores', () => {
   const startUsage = cpuUsage();
   const { cpuUsed } = usage(startUsage)
   expect(cpuUsed).toBeGreaterThan(0);
-  expect(cpuUsed).toBeLessThan(800);
+  expect(cpuUsed).toBeLessThan(100 * cpus().length);
 });
